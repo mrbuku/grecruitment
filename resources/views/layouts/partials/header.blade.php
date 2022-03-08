a64b5c103716f0ab3c1fe2d2a6fb3d2e0d3fe045
Raphael <raphael@Raphaels-MacBook-Air.local>
2022-03-08 16:50:39 +0000

    updates

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header class="site-header fixed w-full z-100">
+    <nav class="flex justify-between h-70 px-4 xl:px-6">
+        <div class="flex">
+            <div class="flex-shrink-0 flex items-center">
+                <a href="{{ url('/') }}" title="{{ __('Home') }}">
+                    <figure class="w-50 min-w-50 text-white">
+                        <span class="inline-svg" style="padding-top: 100%;">
+                            @svg('logo')
+                        </span>
+                    </figure>
+                </a>
+            </div>
+        </div>
+
+        <div class="column flex items-center">
+            @auth
+                @include('layouts.partials.authenticated')
+            @else
+                @include('layouts.partials.guest')
+            @endauth
+
+            <button aria-label="toggle mobile menu" class="burger-button block xl:hidden ml-3 md:ml-4">
+                <span class="burger-button-line pointer-events-none"></span>
+            </button>
+        </div>
+
+    </nav>
+</header>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -3,11 +3,11 @@
 
 @section('body')
 
-    <section class="hero bg-white flex items-center justify-center">
+    <section class="hero bg-white flex items-center justify-center py-20 xl:py-30">
         <img src="{{ asset('img/7451.jpg')  }}" class="object-cover-absolute" alt="hero image">
         <div class="w-full z-100">
             <div class="container">
-                <div class="w-full lh:w-8/12 xl:w-6/12 mx-auto text-center">
+                <div class="w-full lg:w-8/12 xl:w-6/12 mx-auto text-center">
                     <h1 class="text-white mb-4">Hire the world’s best artists in games, film, media & entertainment</h1>
                     <p class="text-white">games artists jobs is the industry’s largest job board for media & entertainment art positions. It enables studios to promote job opportunities to the world’s largest community of professional artists.</p>
                 </div>
